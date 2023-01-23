fe2b869f002db079147b8f1a4897850485f3711b
MadLies <jwilchesc@unal.edu.co>
2023-01-23 02:49:21 -0500

    Final Version

---
+++ Converter/Converter/MauiProgram.cs
@@ -0,0 +1,19 @@
+﻿namespace Converter;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("fontello.ttf", "Icons");
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		return builder.Build();
+	}
+}
